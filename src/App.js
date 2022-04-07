4c10d5de3ee15d5a85fa0674935eb3f91bf797a2
unknown <mairurated@gmail.com>
2022-04-07 10:50:43 +0300

    components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { Route } from "react-router-dom";
+
+import AllMeetupPage from "./pages/AllMeetup";
+import FavouritesPage from "./pages/Favourites";
+import NewMeetupPage from "./pages/NewMeetup";
+import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <MainNavigation />
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupPage />
+        </Route>
+        <Route path="/favourites">
+          <FavouritesPage />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
